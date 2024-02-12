429e6cf0f4f9d1e8fe3df2db4d7be7910adc3e4d
Zuzana Benkova <zuzanka.dvorska@gmail.com>
2024-02-13 00:14:30 +0100

    soubor js

---
+++ script.js
@@ -0,0 +1,28 @@
+const kniha1 = {
+    nazev: "Maly zivot",
+    autor: "Hanya Yanagihara",
+    pocet_stran: Number(630),
+    vydani: {
+        datum_vydani: Number(2015),
+        nakladatelstvi: "Odeon",
+    } 
+};
+const kniha2 = {
+    nazev: "Neklidna krev",
+    autor: "Robert Galbraith",
+    pocet_stran: Number(976),
+    vydani: {
+        datum_vydani: Number(2020),
+        nakladatelstvi: "Kniha Zlin",
+}
+};
+const kniha3 = {
+    nazev: "Černočerné srdce",
+    autor: "Robert Galbraith",
+    pocet_stran: Number(1152),
+    vydani: {
+        datum_vydani: Number(2022),
+        nakladatelstvi: "Kniha Zlin",
+}
+
+};
